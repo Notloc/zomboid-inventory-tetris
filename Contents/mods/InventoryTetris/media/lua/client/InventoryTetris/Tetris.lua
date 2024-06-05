4cf8a117209275af4431ede6f6d3d1065a92010b
Colton Campbell <ColtonJamesCampbell@gmail.com>
2024-06-05 16:33:43 -0300

    Switch mod info out for unlisted workshop version

--- Contents/mods/InventoryTetris/media/lua/client/InventoryTetris/Tetris.lua
+++ Contents/mods/InventoryTetris/media/lua/client/InventoryTetris/Tetris.lua
@@ -5,7 +5,7 @@ local TETRIS_IMG = getTexture("media/textures/Compatibility/tetris.png")
 local EQUIPMENT_IMG = getTexture("media/textures/Compatibility/equipment_ui.png")
 
 InventoryTetris = {
-    version = Version:new(6, 0, 0, "beta")
+    version = Version:new(6, 0, 0, "beta"),
 }
 
 local function showCompatibilityIssues()
@@ -13,6 +13,19 @@ local function showCompatibilityIssues()
         local cpw = CompatibilityPopupWindow:new(100, 100, TETRIS_IMG, InventoryTetris.version, EQUIPMENT_IMG, EquipmentUI and EquipmentUI.version, Version:new(2,0,0))
         cpw:initialise()
         cpw:addToUIManager()
+    end
+
+    local incompatibilityPopup = CompatibilityPopupWindow:new(200, 200, TETRIS_IMG, InventoryTetris.version)
+    incompatibilityPopup:initialise()
+
+    -- Known incompatibilities, I will only list things here that I believe will never be fixed
+    -- and are not worth the effort to work around
+    if getActivatedMods():contains("BetterSortCC") then
+        incompatibilityPopup:addModIncompatibility("Better Sorting", "BetterSortCC")
+    end
+
+    if #incompatibilityPopup.incompatibleMods > 0 then
+        incompatibilityPopup:addToUIManager()
     end
 end
 
--- Contents/mods/InventoryTetris/media/lua/client/Notloc/Versioning/CompatibilityPopupWindow.lua
+++ Contents/mods/InventoryTetris/media/lua/client/Notloc/Versioning/CompatibilityPopupWindow.lua
@@ -19,7 +19,7 @@ local CompatibilityPopupWindow = Window:derive("CompatibilityPopupWindow");
 
 function CompatibilityPopupWindow:new (x, y, sourceImage, sourceVersion, targetImage, targetVersion, minTargetVersion)
     local o = {}
-    o = Window:new(x, y, 256 + 48 + 24*2, 232, "Version Issue Detected");
+    o = Window:new(x, y, 256 + 48 + 24*2, 232, targetImage and "Version Issue Detected" or "Incompatible Mod Detected");
     setmetatable(o, self);
     self.__index = self
 
@@ -33,7 +33,12 @@ function CompatibilityPopupWindow:new (x, y, sourceImage, sourceVersion, targetI
     o.minTargetVersion = minTargetVersion;
 
     o.resizable = false;
+    o.incompatibleMods = {};
     return o;
+end
+
+function CompatibilityPopupWindow:addModIncompatibility(modName, modId)
+    table.insert(self.incompatibleMods, {modName, modId});
 end
 
 function CompatibilityPopupWindow:render()
@@ -47,28 +52,47 @@ function CompatibilityPopupWindow:render()
     local x = PADDING
     local y = TITLEBAR + PADDING
 
-    self:drawTextureScaledAspect(self.sourceImage, x, y, IMG_SIZE, IMG_SIZE, 1, 1, 1, 1);
-    x = x + IMG_SIZE + SPACING
-    self:drawTextureScaledAspect(self.targetImage, x, y, IMG_SIZE, IMG_SIZE, 1, 1, 1, 1);
+    -- Draw version compatibility information
+    if (self.sourceImage and self.targetImage) then
+        self:drawTextureScaledAspect(self.sourceImage, x, y, IMG_SIZE, IMG_SIZE, 1, 1, 1, 1);
+        x = x + IMG_SIZE + SPACING
+        self:drawTextureScaledAspect(self.targetImage, x, y, IMG_SIZE, IMG_SIZE, 1, 1, 1, 1);
 
-    x = PADDING + IMG_SIZE + SPACING/2 - WARNING_SIZE/2
-    y = TITLEBAR + PADDING + IMG_SIZE/2 - WARNING_SIZE/2
-    self:drawTexture(WARNING_TEX, x, y, 1, 1, 1, 1);
+        x = PADDING + IMG_SIZE + SPACING/2 - WARNING_SIZE/2
+        y = TITLEBAR + PADDING + IMG_SIZE/2 - WARNING_SIZE/2
+        self:drawTexture(WARNING_TEX, x, y, 1, 1, 1, 1);
 
-    x = PADDING + IMG_SIZE + SPACING/2 - UNEQUAL_SIZE/2
-    y = TITLEBAR + PADDING + IMG_SIZE/2 - UNEQUAL_SIZE/2 + WARNING_SIZE + 12
-    self:drawTexture(UNEQUAL_TEX, x, y, 1, 1, 1, 1);
+        x = PADDING + IMG_SIZE + SPACING/2 - UNEQUAL_SIZE/2
+        y = TITLEBAR + PADDING + IMG_SIZE/2 - UNEQUAL_SIZE/2 + WARNING_SIZE + 12
+        self:drawTexture(UNEQUAL_TEX, x, y, 1, 1, 1, 1);
 
-    x = PADDING
-    y = TITLEBAR + PADDING + IMG_SIZE + 12
+        x = PADDING
+        y = TITLEBAR + PADDING + IMG_SIZE + 12
 
-    self:drawText("Version: " .. Version.format(self.sourceVersion), x, y, 1, 1, 1, 1, UIFont.Medium);
+        self:drawText("Version: " .. Version.format(self.sourceVersion), x, y, 1, 1, 1, 1, UIFont.Medium);
 
-    x = PADDING + IMG_SIZE + SPACING
-    local targetVersion = self.targetVersion and Version.format(self.targetVersion) or "Unknown"
-    self:drawText("Version: " .. targetVersion, x, y, 1, 0.2, 0.1, 1, UIFont.Medium);
-    y = y + PADDING
-    self:drawText("Required: " .. Version.format(self.minTargetVersion), x, y, 1, 1, 0, 1, UIFont.Medium);
+        x = PADDING + IMG_SIZE + SPACING
+        local targetVersion = self.targetVersion and Version.format(self.targetVersion) or "Unknown"
+        self:drawText("Version: " .. targetVersion, x, y, 1, 0.2, 0.1, 1, UIFont.Medium);
+        y = y + PADDING
+        self:drawText("Required: " .. Version.format(self.minTargetVersion), x, y, 1, 1, 0, 1, UIFont.Medium);
+    else
+        self:setWidth(400)
+        self:setHeight(400)
+
+        self:drawTextureScaledAspect(self.sourceImage, self:getWidth()/2 - IMG_SIZE/2, y, IMG_SIZE, IMG_SIZE, 1, 1, 1, 1);
+        y = y + IMG_SIZE + PADDING
+
+        self:drawText("Incompatible Mod(s) Detected!", x, y, 1, 1, 1, 1, UIFont.Medium);
+        y = y + PADDING * 2
+
+        -- Draw list of mods that are incompatible
+        for i, mod in ipairs(self.incompatibleMods) do
+            self:drawText(mod[1] .. " [ " .. mod[2] .. " ]", x, y, 1, 0.2, 0.1, 1, UIFont.Medium);
+            y = y + PADDING
+        end
+
+    end
 end
 
 return CompatibilityPopupWindow
