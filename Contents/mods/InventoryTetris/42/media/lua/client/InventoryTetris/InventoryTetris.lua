50781d7ba4bef78fbcd5edcab4b5d698b5c39d41
Colton Campbell <ColtonJamesCampbell@gmail.com>
2025-01-25 16:04:49 -0400

    Added version print for logging purposes

--- Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/InventoryTetris.lua
+++ Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/InventoryTetris.lua
@@ -3,3 +3,5 @@ local Version = require("Notloc/Versioning/Version")
 InventoryTetris = {
     version = Version:new(6, 8, 0, "beta"),
 }
+
+print("InventoryTetris version: " .. Version.format(InventoryTetris.version))
