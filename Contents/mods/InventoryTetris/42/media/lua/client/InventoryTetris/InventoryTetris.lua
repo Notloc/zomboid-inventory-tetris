fe220c3e6fc8289d67d27e2439f054c56de2038f
Colton Campbell <ColtonJamesCampbell@gmail.com>
2025-01-26 14:53:45 -0400

    Bumped version

--- Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/InventoryTetris.lua
+++ Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/InventoryTetris.lua
@@ -1,7 +1,7 @@
 local Version = require("Notloc/Versioning/Version")
 
 InventoryTetris = {
-    version = Version:new(6, 8, 0, "beta"),
+    version = Version:new(6, 8, 1, "beta"),
 }
 
 print("InventoryTetris version: " .. Version.format(InventoryTetris.version))
