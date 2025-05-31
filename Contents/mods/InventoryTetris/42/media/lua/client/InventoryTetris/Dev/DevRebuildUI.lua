4730402d3c73cf3ed73d4ef8b06385e34715c512
Colton Campbell <ColtonJamesCampbell@gmail.com>
2025-05-31 15:17:12 -0300

    Improved ui rebuilder script

--- Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/Dev/DevRebuildUI.lua
+++ Contents/mods/InventoryTetris/42/media/lua/client/InventoryTetris/Dev/DevRebuildUI.lua
@@ -4,17 +4,10 @@ Events.OnLoad.Add(function()
     if not TetrisDevTool.isDebugEnabled() then return end
     Events.OnKeyPressed.Add(function (key)
         if key == getKeyCode("R") and isCtrlKeyDown() then
-            local playerData = getPlayerData(0)
+            local player = getSpecificPlayer(0)
 
-            if playerData.playerInventory then
-                playerData.playerInventory:close()
-            end
-
-            if playerData.lootInventory then
-                playerData.lootInventory:close()
-            end
-
-            playerData:createInventoryInterface()
+            destroyPlayerData(player)
+            createPlayerData(0)
         end
     end)
 end)
